3e217325a9a612686c7e6f6c7d3464cab6ebf769
ptrv-dev <petrov99119@gmail.com>
2023-08-17 17:23:00 +0300

    api: sign up

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -16,3 +16,21 @@ export async function signIn(
     return false;
   }
 }
+
+export async function signUp(
+  username: string,
+  email: string,
+  password: string
+): Promise<boolean> {
+  try {
+    const { data } = await $axios.post<ISignInResponse>('/auth/sign-up', {
+      username,
+      email,
+      password,
+    });
+    window.localStorage.setItem('accessToken', data.token);
+    return true;
+  } catch {
+    return false;
+  }
+}
