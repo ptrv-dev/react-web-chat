3647323fbc413a0e5d757e787acac935d48b6b78
ptrv-dev <petrov99119@gmail.com>
2023-08-17 13:17:27 +0300

    shadcn-ui: theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { HomePage } from './pages';
+
+import { HomePage, LoginPage } from './pages';
+import './index.css';
+import { ThemeProvider } from './components/ThemeProvider';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <HomePage />,
   },
+  {
+    path: '/login',
+    element: <LoginPage />,
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider defaultTheme="dark" storageKey="ui-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>
 );
