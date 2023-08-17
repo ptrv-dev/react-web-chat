3e1b6c30c716debc6c84cf0ed2f953d8bcbdf908
ptrv-dev <petrov99119@gmail.com>
2023-08-17 15:12:42 +0300

    home-page: template

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { SendHorizonal } from 'lucide-react';
+
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import Message from '@/components/Message';
 
 const HomePage: React.FC = () => {
-  return <div>HomePage</div>;
+  const [message, setMessage] = useState('');
+  return (
+    <div className="h-full flex flex-col">
+      <div className="p-4 flex flex-col gap-4 items-start h-full overflow-y-auto">
+        <Message username="user" message="Hello world!" time={new Date()} />
+        <Message username="user" message="Hello world!" time={new Date()} />
+        <Message username="user" message="Hello world!" time={new Date()} />
+        <Message username="user" message="Hello world!" time={new Date()} />
+        <Message username="user" message="Hello world!" time={new Date()} />
+        <Message username="user" message="Hello world!" time={new Date()} />
+        <Message username="user" message="Hello world!" time={new Date()} />
+        <Message username="user" message="Hello world!" time={new Date()} />
+        <Message username="user" message="Hello world!" time={new Date()} />
+        <Message username="user" message="Hello world!" time={new Date()} />
+        <Message username="user" message="Hello world!" time={new Date()} />
+        <Message username="user" message="Hello world!" time={new Date()} />
+        <Message username="user" message="Hello world!" time={new Date()} />
+        <Message username="user" message="Hello world!" time={new Date()} />
+        <Message
+          self
+          username="user"
+          message="Hello world!"
+          time={new Date()}
+        />
+      </div>
+      <div className="p-4 bg-gray-950 flex items-center gap-2">
+        <Input
+          placeholder="Enter your message..."
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <Button size={'icon'} className="hover:text-blue-500">
+          <SendHorizonal size={16} />
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
