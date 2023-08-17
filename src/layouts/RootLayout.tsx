c989d3c541d6a37c7d33d9fa4d9ec0f62ff6b8d0
ptrv-dev <petrov99119@gmail.com>
2023-08-17 15:35:40 +0300

    root-layout: refactored

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 import Navbar from '@/components/Navbar';
+import { $axios } from '@/axios';
+import { Loader2 } from 'lucide-react';
 
 const RootLayout: React.FC = () => {
+  const [loading, setLoading] = useState(true);
+  const navigator = useNavigate();
+
+  async function checkLogin() {
+    try {
+      const res = await $axios.get('/auth/me');
+      console.log(res);
+      setLoading(false);
+    } catch {
+      navigator('/sign-in');
+    }
+  }
+
+  useEffect(() => {
+    checkLogin();
+  }, []);
+
+  if (loading)
+    return (
+      <div className="h-screen flex items-center justify-center">
+        <Loader2 className="animate-spin" size={64} />
+      </div>
+    );
+
   return (
     <div className="flex items-stretch h-screen">
       <Navbar />
