77578b02db43f00e2209cb872aad69abd0f88f5f
ptrv-dev <petrov99119@gmail.com>
2023-08-17 15:34:43 +0300

    axios: instace edited

--- src/axios.ts
+++ src/axios.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const token = window.localStorage.getItem('accessToken');
-
 export const $axios = axios.create({
   baseURL: 'http://localhost:5000',
   headers: {
-    Authorization: token ? `Bearer ${token}` : undefined,
+    Authorization: window.localStorage.getItem('accessToken')
+      ? `Bearer ${window.localStorage.getItem('accessToken')}`
+      : undefined,
   },
 });
